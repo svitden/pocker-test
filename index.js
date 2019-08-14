a9b34d5c74529fb6aab3273469baabd19242f2ec
Svitlana Denysenko <s.denysenko@piogroup.net>
2019-08-14 17:21:34 +0300

    finished

--- index.js
+++ index.js
@@ -13,13 +13,6 @@
 
 */
 
-// Проверки до вывода карт:
-// 
-// -- у 90%  игроков есть как минимум одна пара (дополнительная проверка)
-// 3. Подсчитать количество пар у каждого игрока (не больше 2х)
-// 4. Победителю добавить класс css и подсветить парные карты
-
-
 
 Poker = (function($) {
 
@@ -33,7 +26,7 @@ Poker = (function($) {
 
     // *-* public methods *-*
 
-    var init = function() {
+    var init = function() {     
         $(".buttons button").on("click", eventPlayAgainClicked);
     };
 
@@ -94,19 +87,42 @@ Poker = (function($) {
         return document.createElement(tag);
     };
 
-    const appendCards = (id, data) => {
-        console.log(id);
+
+    const findPairs = (arr) => {
+        console.log('arr', arr)
+        const cardPairsArray = arr.map(elem => {            
+            return elem.reduce((acc, cur) => {                
+                acc.push(cur.card);                                        
+                return acc;
+            }, [])
+            .reduce((accum, el) => {                
+                accum[el] = (accum[el] || 0) + 1;                
+                return accum;
+            }, {})
+        });
+        console.log('cardPairsArray', cardPairsArray);
+        return cardPairsArray;  
+    }
+
+
+    const appendCards = (id, data, pairs) => {
+        console.log(pairs);
+        const pairsKeys = Object.keys(pairs);
+        
+
         const constainer = document.getElementById(id);
         const mainDiv = createElement("div");
         mainDiv.classList.add("card-container");
         constainer.append(mainDiv);
                 
-        data.forEach(elem => {            
+        data.forEach(elem => {
+                               
             const card = createElement("div");            
             mainDiv.append(card);
 
             const image = createElement('img');
             image.classList.add("card");
+            image.setAttribute('data-id', `${elem.card}`);
 
             for (let key in elem) {
                 if (elem.hasOwnProperty(key)) {                    
@@ -115,18 +131,57 @@ Poker = (function($) {
             }
                        
             card.append(image);
-        });
+        });        
     };
 
-    const findPairs = (arr) => {
-        console.log(arr);        
+    const colorCards = (id, pairs) => {
+        const pairsKeys = Object.keys(pairs);
+
+        const constainer = document.getElementById(id);
+        const elements = constainer.querySelectorAll('img');
+        console.log(pairsKeys)
+
+        let color1 = 0;
+        let color2 = 0;
+
+        for (let elem of elements) {
+            const cardData = elem.getAttribute('data-id');
+                        
+            if (pairs[cardData] >=2 && pairs[cardData] < 4 && color1 < 2) {
+                
+                elem.classList.add(`pair0`);
+                color1 += 1;
+            } else if (pairs[cardData] >=2 && pairs[cardData] !== 3 && pairs[cardData] < 4 && color2 < 2) {
+                elem.classList.add(`pair1`);
+                color2 += 1;
+            }
+        }
     }
 
+    const showTheWinner = (pairsArray) => {
+        console.log(pairsArray);
+        const numOfPairs = pairsArray.map(elem => {
+            return Object.values(elem).filter(item => item >= 2);             
+        });
+        const [ firstPlayer, secondPlayer ] = numOfPairs;
+        if (firstPlayer.length > secondPlayer.length) {
+            document.getElementById('hand1').classList.add("winning");
+        } else if (firstPlayer.length < secondPlayer.length) {
+            document.getElementById('hand2').classList.add("winning");
+        } 
+    }
+
+    const clear = () => {
+        if (document.getElementByClassName('hand')) {
+            document.getElementById('hand1').innerHtml ='';
+            document.getElementById('hand2').innerHtml ='';
+        }   
+    }
 
     // *-* event methods *-*
 
     var eventPlayAgainClicked = function() {
-        
+                  
         let hands = [];
         // создаем массивы для карт по количеству игроков
         for (let i = 0; i < amountOfPlayers; i += 1) {
@@ -135,47 +190,52 @@ Poker = (function($) {
 
         // 90% игроков - это
         const ninetyPercentOfPlayers = getNinetyPercentOfPlayers(amountOfPlayers);
-        // console.log(ninetyPercentOfPlayers);
+        
 
         //остаток игроков - это
         const restOfPlayers = amountOfPlayers - ninetyPercentOfPlayers;
         
         
-        // выдаем по паре карт для 90 процентов игроков
- 
+        // выдаем по паре карт для 90 процентов игроков 
+
         for (let i = 0; i < ninetyPercentOfPlayers; i += 1) {
             hands[i] = getCardsPair(mainCardArray);
         }
 
-        // додаем оставшиеся карты 90 процентам
-        
-        console.log(mainCardArray)
-        
+        // выдаем оставшиеся карты 90 процентам 
+
         for (let i = 0; i < ninetyPercentOfPlayers; i += 1) {
             for (let j = 0; j < (cardsForEachPlayer - 2); j += 1) {
                 hands[i].push(getRandomCard(mainCardArray))
             }
-            console.log('ninetyPercentOfPlayers', hands[i]);
+            // console.log('ninetyPercentOfPlayers', hands[i]);
         }
 
-        // додаем оставшиеся карты restOfPlayers
+        // выдаем оставшиеся карты restOfPlayers
 
         if (restOfPlayers > 0) {
             for (let i = ninetyPercentOfPlayers; i < amountOfPlayers; i += 1) {
                 for (let j = 0; j < cardsForEachPlayer; j += 1) {
                     hands[i].push(getRandomCard(mainCardArray))
                 }
-                console.log('rest', hands[i]);
+                // console.log('rest', hands[i]);
             }
         }
         
-        console.log(mainCardArray);
+        // console.log(mainCardArray);
 
+        // находим совпадающие пары
+        const pairs = findPairs(hands);
+
+        // Вставляем карты на поле
         for (let i = 0; i < amountOfPlayers; i += 1) {
-            appendCards(`hand${i+1}`, hands[i]);
+            appendCards(`hand${i+1}`, hands[i], pairs[i]);
+            colorCards(`hand${i+1}`, pairs[i]);
         }
 
-        // findPairs(cardArrayHand1);
+        // Определить победителя
+        showTheWinner(pairs);
+
     };
 
     // expose public methods
